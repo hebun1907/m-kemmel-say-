29cbd22dc3933fc1809f48d3c4cd037854e44038
hebun1907 <65681415+hebun1907@users.noreply.github.com>
2020-06-15 12:34:58 +0300

    Add files via upload

---
+++ src/otopark_takip_sistemi/Otopark_takip_sistemi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package otopark_takip_sistemi;
+
+/**
+ *
+ * @author sil
+ */
+public class Otopark_takip_sistemi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
